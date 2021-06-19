5e465ef4a3a6b55566e6822ed57dc36ec2cd4f19
Andrew King <andrewk640@gmail.com>
2021-06-19 21:55:52 +0100

    nwe updates

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const path = require('path');
+const router = express.Router();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname+ '/html/index.html'));
 });
+
+app.use('/', router);
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}!`)
